737f0c5834c2e13f20c2725e52d726555c559990
svladislav00-qq <svladislav00@gmail.com>
2026-02-23 15:15:12 +0300

    corrected the spelling

--- internal/handler/file_handler.go
+++ internal/handler/file_handler.go
@@ -204,13 +204,13 @@ func (h *FileHandler) DownloadObject(c *gin.Context) {
 
 	idStr := c.Param("id")
 	if idStr == "" {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "missing id parametr"})
+		c.JSON(http.StatusBadRequest, gin.H{"error": "missing id parameter"})
 		return
 	}
 
 	id, err := strconv.Atoi(idStr)
 	if err != nil || id <= 0 {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "invalid id parametr"})
+		c.JSON(http.StatusBadRequest, gin.H{"error": "invalid id parameter"})
 		return
 	}
 
